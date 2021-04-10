c667bc5d2679985c5df060b47871b7ea76a4fc62
marco-gaioni <gaionimarco2@gmail.com>
2021-04-10 08:10:52 +0200

    sostituito script casuale con script sensato

--- calls/calls.js
+++ calls/calls.js
@@ -1,4 +1,4 @@
-const express = require("express")
+/*const express = require("express")
 const app = new express()
 const fetch = require("node-fetch")
 const port = 8000
@@ -44,6 +44,251 @@ const attack = () => {
 }
 
 login()
-setInterval(attack,1000)
+setInterval(attack, 1000)
 
-app.listen(port, () => console.log(`app listening on port ${port}`))
+app.listen(port, () => console.log(`app listening on port ${port}`))*/
+
+const express = require("express")
+const app = new express
+const fetch = require("node-fetch")
+
+const port = 8000
+
+const url = "localhost:8080"
+//"93.42.249.207:8080"
+
+
+const username = "pippo"
+const password = "ciao"
+
+const getConditions = (field, cell) => {
+  const upCondition = cell.y > 0 && !field[cell.y - 1][cell.x].hit
+  const downCondition = cell.y < field.length - 1 && !field[cell.y + 1][cell.x].hit
+  const leftCondition = cell.x > 0 && !field[cell.y][cell.x - 1].hit
+  const rightCondition = cell.x < field[cell.y].length - 1 && !field[cell.y][cell.x + 1].hit
+  return { upCondition, downCondition, leftCondition, rightCondition }
+}
+
+const login = async() => {
+  try {
+    let res = await fetch(`http://${url}/signup?team=${username}&password=${password}`)
+    res = await res.status
+    console.log(res)
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const hit = async(cell = null, dir = null) => {
+  let points
+
+  const { gameStatus, completeField, field } = await getField()
+
+  if (!gameStatus) {
+    return { msg: "Tutte le caselle sono state colpite" }
+  }
+
+  if (!cell) {
+    const data = getCasualCell(field, completeField)
+    cell = data.cell
+    dir = data.dir
+  }
+
+  try {
+    const res = await fetch(`http://${url}/fire?x=${cell.x}&y=${cell.y}&team=${username}&password=${password}`)
+    const data = await res.ok ? res.json() : res.status
+    if (res.ok) {
+      points = data.score
+      console.log(await data)
+
+      const { upCondition, downCondition, leftCondition, rightCondition } = getConditions(completeField, cell)
+      if (points === 1) {
+        if (dir === "up" && upCondition) {
+          setTimeout(hit, 1001, completeField[cell.y - 1][cell.x], "up")
+        } else if (dir === "down" && downCondition) {
+          setTimeout(hit, 1001, completeField[cell.y + 1][cell.x], "down")
+        } else if (dir === "left" && leftCondition) {
+          setTimeout(hit, 1001, completeField[cell.y][cell.x - 1], "left")
+        } else if (dir === "right" && rightCondition) {
+          setTimeout(hit, 1001, completeField[cell.y][cell.x + 1], "right")
+        } else {
+          setTimeout(hit, 1001)
+        }
+      } else if (points === 0) {
+        if (dir === "up") {
+          const alive = completeField[cell.y + 1][cell.x].ship ? completeField[cell.y + 1][cell.x].ship.alive : false
+          if (alive) {
+            let newCell = null
+            let y = 1
+            while (!newCell) {
+              newCell = completeField[cell.y + y] ?
+                completeField[cell.y + y][cell.x].hit ? null : completeField[cell.y + y][cell.x] : null
+              if (cell.y + y >= completeField.length) {
+                break
+              }
+              y ++
+            }
+            if (!newCell) {
+              setTimeout(hit, 1001)
+            } else {
+              const condition = getConditions(completeField, newCell).downCondition
+              condition ? setTimeout(hit, 1001, newCell, "down") : setTimeout(hit, 1001)
+            }
+          } else {
+            setTimeout(hit, 1001)
+          }
+        } else if (dir === "down") {
+          const alive = completeField[cell.y - 1][cell.x].ship ? completeField[cell.y - 1][cell.x].ship.alive : false
+          if (alive) {
+            let newCell = null
+            let y = 1
+            while (!newCell) {
+              newCell = completeField[cell.y - y] ?
+                completeField[cell.y][cell.x].hit ? null : completeField[cell.y - y][cell.x] : null
+              if (cell.y - y < 0) {
+                break
+              }
+              y ++
+            }
+            if (!newCell) {
+              setTimeout(hit, 1001)
+            } else {
+              const condition = getConditions(completeField, newCell).upCondition
+              condition ? setTimeout(hit, 1001, newCell, "up") : setTimeout(hit, 1001)
+            }
+          } else {
+            setTimeout(hit, 1001)
+          }
+        } else if (dir === "left") {
+          const alive = completeField[cell.y][cell.x + 1].ship ? completeField[cell.y][cell.x + 1].ship.alive : false
+          if (alive) {
+            let newCell = null
+            let x = 1
+            while (!newCell) {
+              newCell = completeField[cell.y][cell.x + x] ?
+                completeField[cell.y][cell.x + x].hit ? null : completeField[cell.y][cell.x + x] : null
+              if (cell.x + x >= completeField[cell.y].length) {
+                break
+              }
+              x ++
+            }
+            if (!newCell) {
+              setTimeout(hit, 1001)
+            } else {
+              const condition = getConditions(completeField, newCell).rightCondition
+              condition ? setTimeout(hit, 1001, newCell, "right") : setTimeout(hit, 1001)
+            }
+          } else {
+            setTimeout(hit, 1001)
+          }
+        } else if (dir === "right") {
+          const alive = completeField[cell.y][cell.x - 1].ship ? completeField[cell.y][cell.x - 1].ship.alive : false
+          if (alive) {
+            let newCell = null
+            let x = 1
+            while (!newCell) {
+              newCell = completeField[cell.y][cell.x - x] ?
+                completeField[cell.y][cell.x - x].hit ? null : completeField[cell.y][cell.x - x] : null
+              if (cell.x - x < 0) {
+                break
+              }
+              x ++
+            }
+            if (!newCell) {
+              setTimeout(hit, 1001)
+            } else {
+              const condition = getConditions(completeField, newCell).leftCondition
+              condition ? setTimeout(hit, 1001, newCell, "left") : setTimeout(hit, 1001)
+            }
+          } else {
+            setTimeout(hit, 1001)
+          }
+        } else {
+          setTimeout(hit, 1001)
+        }
+      } else {
+        setTimeout(hit, 1001)
+      }
+    } else {
+      console.log(data)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return points
+}
+
+const getCasualCell = (field, completeField) => {
+  let cellShip = completeField.map(row => row.filter(e => e.ship)).filter(e => e.length > 0).flat()
+  cellShip = cellShip.filter(cell => cell.ship.alive)
+  let cell, dir
+  cellShip.forEach(e => {
+    if (e) {
+      const { upCondition, downCondition, leftCondition, rightCondition } = getConditions(completeField, e)
+
+      let count = 0
+      while (true) {
+        const directionList = ["up", "down", "left", "right"]
+        const direction = directionList[Math.floor(Math.random() * directionList.length)]
+
+        if (upCondition && direction === "up") {
+          cell = completeField[e.y - 1][e.x]
+          dir = direction
+          break
+        } else if (downCondition && direction === "down") {
+          cell = completeField[e.y + 1][e.x]
+          dir = direction
+          break
+        } else if (leftCondition && direction === "left") {
+          cell = completeField[e.y][e.x - 1]
+          dir = direction
+          break
+        } else if (rightCondition && direction === "right") {
+          cell = completeField[e.y][e.x + 1]
+          dir = direction
+          break
+        } else if (count > 20) {
+          break
+        }
+        count ++
+      }
+      if (cell) {
+        return cell
+      }
+    }
+  })
+  if (!cell) {
+    const yrandom = Math.floor(Math.random() * field.length)
+    const xrandom = Math.floor(Math.random() * field[yrandom].length)
+    cell = field[yrandom][xrandom]
+    dir = null
+  }
+  return { cell, dir }
+}
+
+const getField = async() => {
+  let completeField
+
+  try {
+    let res = await fetch(`http://${url}/?format=json`)
+    res = await res.json()
+    completeField = res.field
+  } catch (err) {
+    console.error(err)
+  }
+
+  const tempField = completeField.map(row => row.filter(cell => !cell.hit))
+  const field = tempField.filter(row => row.length > 0)
+  const gameStatus = !tempField.every(row => row.every(cell => !cell))
+
+  if (gameStatus) {
+    return { gameStatus, completeField, field }
+  } else {
+    return { gameStatus }
+  }
+}
+
+login()
+hit()
+
+app.listen(port, () => console.log("Listening on port", port))
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+/*const express = require("express")
 const app = new express()
 
 app.use(express.json())
@@ -128,7 +128,7 @@ app.post("/signup", ({ body: { team, password } }, res) => {
         res.status(400).send({ msg: `hai già effettuato l'accreditamento con il nome:  ${team}` })
     } else {
         teams[team] = {
-            team, password, score: 0, killedShips: [], firedBullets: 0, lastFiredBullet: new Date().getTime()
+            team, password, score: 0, killedShips: [], firedBullets: 0, lastFiredBullet: 0
         }
         res.status(200).send({ msg: "Accreditamento riuscito!", credentials: { team, password } })
     }
@@ -136,6 +136,15 @@ app.post("/signup", ({ body: { team, password } }, res) => {
 
 app.post("/fire", ({ body: { x, y, team, password } }, res) => {
     if (teams[team].password === password) {
+
+        let now = new Date().getTime()
+        if (now - teams[team].lastFiredBullet < 1000){
+            return res.status(408).send({ msg: "Too much calls" })
+        }
+
+        teams[team].firedBullets++
+        teams[team].lastFiredBullet = now
+
         if (x > w || y > h || x < 0 || y < 0) {
             teams[team].score -= 20
             return res.status(400).send({ score: teams[team].score, msg: "Sei uscito dal campo!" })
@@ -173,4 +182,350 @@ app.post("/fire", ({ body: { x, y, team, password } }, res) => {
     }
 })
 
-app.listen(port, () => console.log("Listening on port", port))
+app.listen(port, () => console.log("Listening on port", port))*/
+
+
+const faker = require("faker")
+const express = require("express")
+const app = new express()
+const { join } = require("path")
+require("dotenv").config()
+
+const teams = {}
+const field = []
+const ships = []
+
+const PORT = Number(process.env.PORT) || 8080
+const W = Number(process.env.W) || 50
+const H = Number(process.env.H) || 50
+const S = Number(process.env.S) || 10
+const FIRE_LIMIT = Number(process.env.FIRE_LIMIT) || 10
+
+const gameStatus = {
+  active: true,
+  startTime: new Date().getTime(),
+  endTime: null
+}
+
+for (let y = 0; y < H; y++) {
+  const row = []
+  for (let x = 0; x < W; x++) {
+    row.push({
+      team: null,
+      x,
+      y,
+      ship: null,
+      hit: false
+    })    
+  } 
+  field.push(row)
+}
+
+let id = 1
+for (let i = 0; i < S; i++) {
+  const maxHp = faker.random.number({ min: 1, max: 6 })
+  const vertical = faker.random.boolean()
+
+  const ship = {
+    id,
+    name: faker.name.firstName(),
+    x: faker.random.number({ min: 0, max: vertical ? W - 1 : W - maxHp }),
+    y: faker.random.number({ min: 0, max: vertical ? H - maxHp : H - 1 }),
+    vertical,
+    maxHp,
+    curHp: maxHp,
+    killer: null
+  }
+
+  let found = false
+  for (let e = 0; e < ship.maxHp; e++) {
+    const x = ship.vertical ? ship.x : ship.x + e
+    const y = ship.vertical ? ship.y + e : ship.y
+    if (field[y][x].ship) {
+      found = true
+      break
+    }
+  }
+
+  if (!found) {
+    console.log({ vertical, maxHp })
+    for (let e = 0; e < ship.maxHp; e++) {
+      const x = ship.vertical ? ship.x : ship.x + e
+      const y = ship.vertical ? ship.y + e : ship.y
+      field[y][x].ship = ship
+    }
+  
+    ships.push(ship)
+    id ++
+  }
+}
+
+const getVisibleField = () => {
+  return field.map(row => row.map(cell => ({ 
+    x: cell.x,
+    y: cell.y,
+    hit: cell.hit,
+    team: cell.team,
+    ship: cell.hit ? 
+      cell.ship ? { 
+        id: cell.ship.id, 
+        name: cell.ship.name, 
+        alive: cell.ship.curHp > 0,
+        killer: cell.ship.killer 
+      } : null 
+      : null
+  })))
+}
+
+const getVisibleShip = () => {
+  return ships.map(ship => ({
+    id: ship.id,
+    name: ship.name,
+    alive: ship.curHp > 0,
+    killer: ship.killer
+  }))
+}
+
+const getScores = () => {
+  return Object.values(teams).map(({
+    name,
+    score,
+    killedShips,
+    firedBullets,
+    lastFiredBullet,
+  }) => ({
+    name,
+    score,
+    killedShips,
+    firedBullets,
+    lastFiredBullet
+  }))
+}
+
+const renderField = visibleField => {
+  return `
+    <table>
+      <tbody>
+        ${visibleField.map(
+          row => `
+          <tr>
+            ${row.map(cell => `
+              <td class="${[
+                cell.hit ? "hit" : null,
+                cell.hit && cell.ship ? "ship" : null,
+                /*cell.ship?.killer ? "killed" : null*/ cell.ship? (cell.ship.killer? "killed" : null) : null
+              ].filter(e => e).join(" ")}">${cell.ship ? cell.team.name : ""}</td>`
+            ).join("")}
+          </tr>`)
+          .join("")}
+      </tbody>
+    </table>
+  `
+}
+
+const renderScores = score => {
+  return `
+    <h1>SCORES</h1>
+    <ol>
+      ${score
+        .sort((a, b) => b.score - a.score)
+        .map(({ name, score, firedBullets, killedShips }) => `<li>${name} - score: ${score} - kills: ${killedShips.length} - fired bullets: ${firedBullets}</li>`)
+        .join("")
+      }
+    </ol>
+  `
+}
+
+app.use(express.static(join(__dirname, "./public")))
+
+app.get("/", ({ query: { format } }, res) => {
+  const visibleField = getVisibleField()
+
+  if ( format === "json") {
+    res.json({ 
+      field: visibleField,
+      ships: getVisibleShip()
+    })
+  } else {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Naval warfare</title>
+      <style>
+        body {
+          color: white;
+          font-size: 20px;
+          font-family: monospace;
+          white-space: nowrap; 
+          text-shadow: 2px 2px black;
+          text-align: center;
+        }
+        h1 {
+          font-size: 30px;
+        }
+        #field {
+          white-space: nowrap;
+        }
+        #score {
+          position: absolute;
+          top: 0;
+          left: 0;
+          margin: 20px;
+          padding: 20px;
+          background-color: rgba(0,0,0,0.8);
+        }
+        td {
+          border: 1px solid black;
+          width: 100px;
+          height: 100px;
+          display: inline-block;
+          background-size: 150px;
+          background-image: url(./water.gif);
+          color: white;
+          font-family: monospace;
+        }
+        td.hit.ship {
+          filter: hue-rotate(165deg);
+        }
+        td.hit.ship.killed {
+          background-image: url(./explosion.gif);
+          background-size: 100px;
+          filter: hue-rotate(0deg);
+        }
+        td.hit {
+          filter: sepia(1);
+        }
+        
+        table {
+          width: 100%;
+          border-collapse: collapse;
+        }
+      </style>
+    </head>
+    <body>
+      <div id="field">
+        ${renderField(visibleField)}
+      </div>
+      <div id="score">
+        ${renderScores(getScores())}
+      </div>
+      <script src="./script.js"></script>
+    </body>
+    </html>
+    `)
+  }
+})
+
+app.get("/getFieldHtml", (req, res) => {
+  res.send(renderField(getVisibleField()))
+})
+
+app.get("/getScoresHtml", (req, res) => {
+  res.send(renderScores(getScores()))
+})
+
+app.get("/scores", (req, res) => {
+  res.json(getScores())
+})
+
+app.get("/signup", ({ query: { team: teamName, password } }, res) => {
+  if (!teamName || !password) {
+    return res.sendStatus(400)
+  }
+  
+  if (teams[teamName]) {
+    return res.sendStatus(409)
+  }
+
+  teams[teamName] = {
+    name: teamName,
+    password,
+    score: 0,
+    killedShips: [],
+    firedBullets: 0,
+    lastFiredBullet: 0
+  }
+
+  res.sendStatus(200)
+})
+
+app.get("/fire", ({ query: { x, y, team: teamName, password } }, res) => {  
+  if (!gameStatus.active) {
+    return res.sendStatus(400)
+  }
+
+  const team = teams[teamName]
+  if (!team || team.password !== password) {
+    return res.sendStatus(401)
+  }
+  
+  const now = new Date().getTime()
+  if (now - team.lastFiredBullet < FIRE_LIMIT) {
+    team.lastFiredBullet = now
+    return res.sendStatus(429)
+  } else {
+    team.lastFiredBullet = now
+  }
+
+  team.firedBullets ++
+
+  let message, score
+  
+  x = Number(x)
+  y = Number(y)
+
+  if (isNaN(x) || isNaN(y) || x >= W || x < 0 || y >= H || y < 0) {
+    message = "OUT_OF_FIELD"
+    score = -10
+  } else { 
+    const cell = field[y][x]
+
+    if (cell.hit) {
+      message = "ALREADY_HIT"
+      score = -5
+    } else {
+      cell.hit = true
+      cell.team = team
+
+      if (!cell.ship) {
+        message = "WATER"
+        score = 0
+      } else {
+        cell.ship.curHp --
+        if (cell.ship.curHp > 0) {
+          message = "HIT"
+          score = 10
+        } else {
+          const { name, id, maxHp } = cell.ship
+          cell.ship.killer = team
+          team.killedShips.push({
+            name,
+            id,
+            maxHp
+          })
+          message = "KILL"
+          score = 50
+
+          if (ships.every(({ curHp }) => curHp === 0)) {
+            gameStatus.active = false
+            gameStatus.endTime = new Date().getTime()
+          }
+        }
+      }
+    } 
+  }
+
+  res.json({
+    message,
+    score
+  })
+
+  team.score += score
+})
+
+app.all("*", (req, res) => {
+  res.sendStatus(404)
+})
+
+app.listen(PORT, () => console.log("App listening on port %O", PORT))
